45bea2684217ffdc49670dee6614a0f6196133cd
xalfiandwikax <xalfiandwikax@gmail.com>
2025-05-27 02:24:50 +0700

    update docker

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:password@mongodb:27017')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/api', (req, res) => {
+  res.json({ message: "Hello from backend!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Backend running on port ${port}`);
 });
---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+   server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true
+      }
+    }
+  }
+});
